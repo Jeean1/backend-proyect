c753d69c4729c761dc2dfb69cd4e32c17c62049e
ljeanc <jgiraldo@in-ui.com>
2025-01-17 16:14:39 -0500

    add start code

---
+++ src/routes/controllers/auth/index.js
@@ -0,0 +1,7 @@
+export async function activeController(req, res) {
+
+
+
+    return res.status
+
+}
---
+++ src/server.js
@@ -0,0 +1,18 @@
+require('dotenv').config(); // Carga las variables del archivo .env
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT;
+
+// Middleware para analizar JSON
+app.use(express.json());
+
+
+// Ruta principal
+app.get("/", (req, res) => {
+  res.send("¡Hola, mundo desde Node.js!");
+});
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+  console.log(`🚀🚀 Server at http://localhost:${PORT} 🚀🚀`);
+});
